9a39aae189d2009b74d4fdd73e89226ee0f3e90f
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2018-01-23 10:43:07 -0200

    Criando a aula 01

---
+++ ang01-hello-world/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovoComponenteComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ang01-hello-world/src/app/novo-componente/novo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'novo-componente-component',
+    template: `
+        <p>Meu primeiro componente!</p>
+    `
+})
+export class NovoComponenteComponent {
+
+}
