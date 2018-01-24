0512776d2e4e2dc9e6699efb07c13dc92eefa16d
Rodrigo Nappi <rodrigo.nappi@engage.bz>
2018-01-24 13:28:02 -0200

    Aula05 - Criando módulos

--- ang01-hello-world/src/app/app.module.ts
+++ ang01-hello-world/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { NovoComponenteComponent } from './novo-componente/novo-componente.compo
     NovoComponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
